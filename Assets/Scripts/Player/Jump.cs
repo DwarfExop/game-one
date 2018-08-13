7345f9b262804b81b335df67ebff282ac76d7579
jjvdt <jellejanvdt@hotmail.nl>
2018-08-13 19:37:52 +0200

    Made basic jumping + UI
    Made a script for basic jumping and made toggleable UI elements

--- Assets/Scripts/Player/Jump.cs
+++ Assets/Scripts/Player/Jump.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Jump : MonoBehaviour {
+    public float JumpStrength = 10;
 
+    private bool canJump = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,9 +13,11 @@ public class Jump : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space) == true)
+		if(Input.GetKeyDown(KeyCode.Space))
         {
-            
+            Debug.Log("Memebeer");
+            GetComponent<Rigidbody>().AddForce(Vector3.up * JumpStrength,ForceMode.Impulse);
         }
 	}
+
 }
---
+++ Assets/Scripts/UI/PanelToggler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelToggler : MonoBehaviour {
+
+    public KeyCode keyCode;
+    public List<GameObject> Elements;
+
+    private bool toggled = false;
+
+	void Update () {
+        if (Input.GetKeyDown(keyCode) == true)
+        {
+            ToggleInterfaces();
+        }
+
+    }
+    public void ToggleInterfaces()
+    {
+        toggled = toggled ? false : true;
+        foreach (GameObject gameObject in Elements)
+        {
+            gameObject.SetActive(toggled);
+        }
+    }
+}

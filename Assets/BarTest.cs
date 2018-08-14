95324812ab3f2b974e25116dadbcfafccee0d4f2
jjvdt <jellejanvdt@hotmail.nl>
2018-08-14 18:08:38 +0200

    Added Hp bar and made prefab
    Added some sprites for UI and added hp bar and layout for it, also made some logic for management of the bars

---
+++ Assets/BarTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarTest : MonoBehaviour {
+
+    public Slider healthBar;
+
+    [Range(0,200)]
+    public float CurrentValue = 100;
+    public float MaxValue = 100;
+    public string Represents;
+    private float maxSize;
+    private Text text;
+
+    private void Start()
+    {
+        healthBar.value = CalculateHealth();
+        text = healthBar.GetComponentInChildren<Text>(true);
+    }
+    // Update is called once per frame
+    void Update () {
+        healthBar.value = CalculateHealth();
+        text.text = string.Format("{0}: {1} / {2}",Represents,CurrentValue,MaxValue);
+	}
+
+    float CalculateHealth()
+    {
+        return CurrentValue / MaxValue;
+    }
+
+}

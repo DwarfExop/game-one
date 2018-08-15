03bffcbc0d957bf1d313540e91998738111371d4
jjvdt <jellejanvdt@hotmail.nl>
2018-08-15 23:15:51 +0200

    Added infinite jump fixed
    Fixed the problem that the player can spam the spacebar and jump infinite times

--- Assets/Scripts/Player/Jump.cs
+++ Assets/Scripts/Player/Jump.cs
@@ -6,20 +6,31 @@ public class Jump : MonoBehaviour {
 
     [Range(1,20)]
     public float JumpStrength = 10;
+    private bool canJump = true;
 
-    private bool canJump = false;
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if(Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Memebeer");
-            GetComponent<Rigidbody>().AddForce(Vector3.up * JumpStrength,ForceMode.Impulse);
+            if (canJump)
+            {
+                Debug.Log("Memebeer");
+                GetComponent<Rigidbody>().AddForce(Vector3.up * JumpStrength, ForceMode.Impulse);
+                canJump = false;
+            }
+
         }
 	}
 
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Collision");
+        canJump = true;
+    }
 }

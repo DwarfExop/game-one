5af4d458bdca9158472bb3ca3204b1289967eac6
jjvdt <jellejanvdt@hotmail.nl>
2018-08-16 15:35:04 +0200

    Added cooldown and base Action method for the ability base class

--- Assets/Prefabs/ScriptableObjects/AbilityBaseClass.cs
+++ Assets/Prefabs/ScriptableObjects/AbilityBaseClass.cs
@@ -7,4 +7,11 @@ public class Ability: ScriptableObject {
     public string abilityName;
     public string abilityDescription;
 
+    public float cooldown;
+
+    protected virtual void AbilityAction()
+    {
+        Debug.Log(string.Format("Ability: {0} was used",abilityName));
+    }
+
 }
